6c2615e0b0ba59978455bfee879bdae1d424e0cc
Dion Moult <dion@thinkmoult.com>
2013-07-08 15:29:38 +1000

    Create views for voting ajax calls

---
+++ application/classes/View/Comment/Vote.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+class View_Comment_Vote extends View_Layout
+{
+    public function status()
+    {
+        return $this->status;
+    }
+}
